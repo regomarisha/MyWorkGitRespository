0bf02c62f3ecbfa7279e6868d9e44bdda84eb265
Marisha <ra***@gmail.com>
2019-05-23 10:22:31 -0700

    s

---
+++ Hai.java
@@ -0,0 +1,7 @@
+package wbl.edu.basic;
+
+public class Hai {
+    public static void main(String[] args) {
+        System.out.println("Hai world");
+    }
+}
---
+++ hello.java
@@ -0,0 +1,7 @@
+package wbl.edu.basic;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World , welcome to java world :)");
+    }
+}
