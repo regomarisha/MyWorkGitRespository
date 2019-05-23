1a83b06f5d7385659ad56cc26cb4e382ff6fbf8f
regomarisha <regomarisha@gmail.com>
2019-05-23 10:43:40 -0700

    Update hello.java

--- src/wbl/edu/basic/hello.java
+++ src/wbl/edu/basic/hello.java
@@ -1,6 +1,6 @@
 package wbl.edu.basic;
 
-public Class hello {
+public class hello {
     public static void main(String[] args) {
         System.out.println("Hello World , welcome to java world :)");
     }
